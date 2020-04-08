f855d250f8f2453259f2ac84717c399e39be5ef8
atenasadat <atenasaghi@yahoo.com>
2020-04-08 16:36:46 +0430

    Initial commit

---
+++ app/src/main/java/com/example/weatherforecast/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.weatherforecast;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private ProgressBar progressBar ;
+    private Button showbtn ;
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ////progressbar
+        init();
+        progressBar.setVisibility(View.VISIBLE);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                progressBar.setVisibility(View.GONE);
+            }
+        } ,10000);
+
+        /////button
+        showbtn = (Button) findViewById(R.id.showbutton);
+//        showbtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this , second_activity.class);
+//                startActivity(intent);
+//            }
+//        });
+
+
+        ///listview
+
+        listView = findViewById(R.id.listview);
+
+
+
+    }
+
+    private void init() {
+        this.progressBar=findViewById(R.id.progressbar);
+    }
+}
